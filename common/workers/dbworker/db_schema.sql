b99942aef3f60bf4f2fc219bf19a5ec8848b745e
Raj Vimal Chopra <raj.chopra10@gmail.com>
2016-05-15 02:15:55 +0530

    Updated db_schema

--- common/workers/dbworker/db_schema.sql
+++ common/workers/dbworker/db_schema.sql
@@ -13,7 +13,7 @@ minion_lat VARCHAR(10),
 minion_long VARCHAR(10),
 minion_priority INT(2),
 minion_rating decimal(2,2),
-timestamp TIMESTAMP,
+date_registered DATETIME,
 CONSTRAINT pk_minion_id PRIMARY KEY (minion_id)
 );
 
@@ -29,7 +29,7 @@ merchant_lat VARCHAR(10),
 merchant_long VARCHAR(10),
 merchant_priority INT(2),
 merchant_rating decimal(2,2),
-timestamp TIMESTAMP,
+date_registered DATETIME,
 CONSTRAINT pk_merchant_id PRIMARY KEY (merchant_id)
 );
 
@@ -44,7 +44,7 @@ customer_addr VARCHAR(300),
 customer_lat VARCHAR(10),
 customer_long VARCHAR(10),
 customer_rating decimal(2,2),
-timestamp TIMESTAMP,
+date_registered DATETIME,
 CONSTRAINT pk_customer_id PRIMARY KEY (customer_id)
 );
 
@@ -58,10 +58,13 @@ delivery_id VARCHAR(16) NOT NULL,
 merchant_id INT(10) NOT NULL,
 customer_id  INT(10),
 minion_id INT(10),
-current_status VARCHAR(50),
-timestamp TIMESTAMP,
+status VARCHAR(50),
+time_created DATETIME,
+time_completed DATETIME,
+time_updated DATETIME,
 CONSTRAINT pk_delivery_id PRIMARY KEY (delivery_id)
 );
+
 
 DROP TABLE activity_log;
 
